084cd3c2a1d2a30a0ac5f5882d255b06758ba462
nobuyaiwao <nobuyaiwao@gmail.com>
2020-03-02 15:55:59 +0900

    morgan

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const cool = require('cool-ascii-faces')
+const morgan = require('morgan');
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(morgan('combined'))
+  .use(bodyParser.urlencoded({ extended: false }))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
